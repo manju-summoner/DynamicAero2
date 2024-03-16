f5535dcb2b57be9bcbf7de113adb41694ebffec9
manju-summoner <manju_summoner@manjubox.net>
2024-03-17 02:21:00 +0900

    Add DataGrid cell style example

---
+++ DynamicAero2.Sample/DataGridItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DynamicAero2.Sample
+{
+    internal class DataGridItem
+    {
+        public bool? Check { get; set; }
+        public string Text { get; set; }
+        public Key ComboBox { get; set; }
+
+        public static DataGridItem[] Source { get; } = new[]
+        {
+            new DataGridItem { Check = true, Text = "Item 1", ComboBox = Key.A },
+            new DataGridItem { Check = null, Text = "Item 2", ComboBox = Key.B },
+            new DataGridItem { Check = false, Text = "Item 3", ComboBox = Key.C },
+        };
+        public static Key[] KeySource { get; } = Enum.GetValues(typeof(Key)).Cast<Key>().ToArray();
+    }
+}
